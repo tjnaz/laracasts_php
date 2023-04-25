827d1818e9640d61c9927ffaae10ea19abdf174a
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-25 17:02:34 +0530

    finish: 7 ass. array

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -10,8 +10,28 @@
 
 <body>
   <?php
-    $books = ["Do Android Dream of Electric Sheep?", "The Langoliers", "Hail Mary"];
-    ?>
+
+    $books = [
+    [
+    'name' => "Do Android Dream of Electric Sheep?",
+    'author' => 'Philip K. Dick',
+    'purchaseURL' => "https://www.amazon.com/Blade-Runner-Philip-K-Dick/dp/1524796972/",
+    ],
+
+    [
+    'name' => "Dark Matter",
+    'author' => 'Blake Crouch',
+    'purchaseURL' => "https://www.amazon.com/Dark-Matter-Novel-Blake-Crouch/dp/1101904240",
+    ],
+
+    [
+    'name' => "Project Hail Mary",
+    'author' => 'Andy Weir',
+    'purchaseURL' => "https://www.amazon.com/Project-Hail-Mary-Andy-Weir/dp/0593135202/",
+    ],
+    ];
+
+  ?>
 
   <h1>
     Book Recommendations
@@ -20,14 +40,14 @@
   <ul>
     <?php foreach ($books as $book) : ?>
     <li>
-        <?php echo $book ?>
+      <a href="<?= $book['purchaseURL'] ?>">
+        <?php echo "Name: " . $book['name'] ?>
+        <?php echo "Author: " . $book['author'] ?>
+      </a>
     </li>
     <?php endforeach; ?>
   </ul>
 
-  <p>
-    <?php echo $books[1]; ?>
-  </p>
   <h2><a href="./hw.html">Homework Directory</a></h2>
 </body>
 
