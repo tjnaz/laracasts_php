87536989ec4289abb7291dc5d80679b8a8ddaa1b
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-25 20:23:37 +0530

    add: release date of books

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -15,19 +15,29 @@
     [
     'name' => "Do Android Dream of Electric Sheep?",
     'author' => 'Philip K. Dick',
+    'releaseYear' => 1968,
     'purchaseURL' => "https://www.amazon.com/Blade-Runner-Philip-K-Dick/dp/1524796972/",
     ],
 
     [
     'name' => "Dark Matter",
     'author' => 'Blake Crouch',
+    'releaseYear' => 2016,
     'purchaseURL' => "https://www.amazon.com/Dark-Matter-Novel-Blake-Crouch/dp/1101904240",
     ],
 
     [
     'name' => "Project Hail Mary",
     'author' => 'Andy Weir',
+    'releaseYear' => 2021,
     'purchaseURL' => "https://www.amazon.com/Project-Hail-Mary-Andy-Weir/dp/0593135202/",
+    ],
+
+    [
+    'name' => "The Martian",
+    'author' => 'Andy Weir',
+    'releaseYear' => 2011,
+    'purchaseURL' => "https://www.amazon.com/Martian-Novel-Andy-Weir/dp/0804139024",
     ],
     ];
 
